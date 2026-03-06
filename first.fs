85a14a81e866442017aa684e6bb0930c5fd14a7c
sixqwera <nikcharushnikov@gmail.com>
2026-03-06 09:59:09 +0500

    Update first.fs

--- first.fs
+++ first.fs
@@ -7,7 +7,7 @@ let main argv =
     let numbers = [1; 2; 3; 4; 5; 6; 7; 8; 9;]
     let after_toBin = numbers  |> List.map toBin
 
-    printfn "исла %A" numbers
+    printfn "Числа %A" numbers
     printfn "Двоичная запись чисел %A" after_toBin
 
     0
