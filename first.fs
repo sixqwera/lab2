d34d605fa1904cf541ba86e9834b46a0e3981eea
sixqwera <nikcharushnikov@gmail.com>
2026-03-06 10:23:32 +0500

    Update first.fs

--- first.fs
+++ first.fs
@@ -1,11 +1,25 @@
 open System
 
-let toBin(n: int) = Convert.ToString(n,2)
+let rec getlist() = 
+    let s = Console.ReadLine()
+    if String.IsNullOrWhiteSpace(s) then 
+        []
+    else 
+        if Char.IsDigit(s.[0]) then 
+            let n = int s 
+            n :: getlist()
+        else
+            printfn "Ошибка, букв не должно быть" 
+            getlist()
+
+let toBin(n: int) = Convert.ToString(n, 2)
 
 [<EntryPoint>]
 let main argv = 
-    let numbers = [1; 2; 3; 4; 5; 6; 7; 8; 9;]
-    let after_toBin = numbers  |> List.map toBin
+    printfn "Вводите числа:"
+    
+    let numbers = getlist()
+    let after_toBin = numbers |> List.map toBin
 
     printfn "Числа %A" numbers
     printfn "Двоичная запись чисел %A" after_toBin
